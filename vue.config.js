60c676ae4071ffa4d7483d05a459b9527b3db4ec
高英豪 <18600408534@163.com>
2019-12-06 09:21:16 +0800

    new

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+// vue.config.js
+module.exports = {
+  baseUrl: '',
+  devServer: {
+    // proxy: 'http://192.168.37.74:8889',
+    // proxy: 'http://221.2.140.133:8601/',
+    proxy: 'http://221.2.140.133:8099',
+  },
+};
